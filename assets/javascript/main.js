ccf2abb97d7b9fcbf7317852731af45bc9ea54fc
alexandre gomes <alecxandy@gmail.com>
2022-08-20 01:18:28 -0300

    version 1.0

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,44 @@
 function criptografar(texto) {
   return texto
-    .replace("e", "enter")
-    .replace("i", "imes")
-    .replace("a", "ai")
-    .replace("o", "ober")
-    .replace("u", "ufat");
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
 }
 
 function descriptografar(texto) {
   return texto
-    .replace("enter", "e")
-    .replace("imes", "i")
-    .replace("ai", "a")
-    .replace("ober", "o")
-    .replace("ufat", "u");
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
 }
 
 function aplicarCriptografia() {
   var texto = document.getElementById("textarea").value;
-  if (texto != " ") {
+  if (texto != "") {
     document.getElementById("textarea").value = "";
     document.getElementById("textarea2").value = criptografar(texto);
+    document.getElementById("textarea2").style.backgroundImage = "none";
   }
 }
 
 function aplicarDescriptografia() {
   var texto = document.getElementById("textarea2").value;
-  if (texto != " ") {
-    document.getElementById("textarea2").value = " ";
+  if (texto != "") {
     document.getElementById("textarea").value = descriptografar(texto);
+    document.getElementById("textarea2").value = "";
+  }
+}
+
+function copiarTexto() {
+  var content = document.getElementById("textarea2");
+  if (content.value != "") {
+    content.select();
+    content.setSelectionRange(0, 99999); /* For mobile devices */
+    navigator.clipboard.writeText(content.value);
+    alert(" Copiado com sucesso! " + content.value);
   }
 }
